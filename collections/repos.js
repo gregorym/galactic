fa8973062e149688123b2b8f3a4c703f2309263b
Gregory Marcilhacy <greg@cruiswise.com>
2012-08-16 17:11:57 -0700

    Adding repos collection

---
+++ collections/repos.js
@@ -0,0 +1,21 @@
+(function() {
+  var app = this;
+  app.collections = app.collections || {};
+
+  app.collections.Repos = Backbone.Collection.extend({
+    model: app.models.Repo,
+    url: ('https://api.github.com/users/' + app.current_user + '/watched'),
+
+    privateRepos: function() {
+      return _.filter(models, function(repo){
+        return (repo.private === true)
+      });
+    },
+
+    publicRepos: function() {
+      return _.filter(models, function(repo){
+        return (repo.private === false)
+      });
+    }
+  });
+}).apply(starboard);
