fc5c061f4eaf8970eb5149c3cc60c887ea974e6e
Gregory Marcilhacy <greg@cruiswise.com>
2012-08-22 13:49:42 -0700

    push dummy value is an array after each fetch

--- collections/repos.js
+++ collections/repos.js
@@ -1,14 +1,17 @@
 (function() {
   var app = this;
   app.collections = app.collections || {};
+  var LAST_PAGE;
+  var FETCHED_ARRAY = [];
 
   app.collections.Repos = Backbone.Collection.extend({
     model: app.models.Repo,
-    initialize: function(max_page){
+    initialize: function(last_page){
+      LAST_page = last_page;
       this.url = ('https://api.github.com/users/' + app.current_user + '/watched');
 
-      for (var i = 1; i <= max_page; i++) {
-        this.fetch({add: true, data: {page: i}})  
+      for (var i = 1; i <= LAST_PAGE; i++) {
+        this.fetch({add: true, data: {page: i}, success: function(){ FETCHED_ARRAY.push(true) });  
       };
     },
 
