f59691fa60b282f841825ed74bdb4ac7023f90ce
Gregory Marcilhacy <greg@cruiswise.com>
2012-08-28 15:15:23 -0700

    typo

--- app/collections/tags.js
+++ app/collections/tags.js
@@ -2,7 +2,7 @@
   var app = this;
   app.collections = app.collections || {};
   
-  app.collections.Repos = Backbone.Collection.extend({
+  app.collections.Tags = Backbone.Collection.extend({
     model: app.models.Tag,
     localStorage: new Backbone.LocalStorage("galactic_tags")    
   });
